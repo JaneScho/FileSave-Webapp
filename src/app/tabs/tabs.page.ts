a5669b385df09f128eeb0ab7d14838bddd80ab8e
jschophu <jannes.schophuis@hs-osnabrueck.de>
2026-02-20 18:58:22 +0100

    Navbar Aufbau

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,28 +1,29 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonFabList, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonFab, IonFabButton } from '@ionic/angular/standalone';
+import { IonFabList, IonTabs, IonTabBar, IonTabButton, IonIcon, IonMenu, IonLabel, IonFab, IonFabButton, IonButton, IonMenuButton, IonButtons, IonList, IonContent } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { closeSharp, pushOutline, cameraOutline, listOutline, imagesOutline, image, folder, shareSocial, search, settings, cloudUploadOutline } from 'ionicons/icons';
+import { closeSharp, pushOutline, cameraOutline, listOutline, imagesOutline, image, folder, shareSocial, search, settings, cloudUploadOutline, barChartOutline, settingsOutline, menuOutline, folderOutline, searchOutline, shareSocialOutline, } from 'ionicons/icons';
 import { CameraService } from '../services/camera.service';
 import { LocationService } from '../services/location.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonFabList, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonFab, IonFabButton],
+  imports: [RouterLinkActive, RouterLink, IonFabList, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonFab, IonFabButton, IonButton, IonMenuButton, IonButtons, IonList, IonContent, IonMenu],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor(public cameraService : CameraService, public locationService : LocationService) {
-    addIcons({ closeSharp, pushOutline, cameraOutline, listOutline, imagesOutline, image, folder, shareSocial, search, settings, cloudUploadOutline });
+  constructor(public cameraService: CameraService, public locationService: LocationService) {
+    addIcons({ imagesOutline, menuOutline, searchOutline, folderOutline, closeSharp, pushOutline, cameraOutline, listOutline, shareSocialOutline, image, folder, shareSocial, search, settings, cloudUploadOutline });
   }
 
-  async takeNewPhoto(){
+  async takeNewPhoto() {
     const location = await this.locationService.getLocationName();
 
-    console.log('Current location: ', location )
-    
+    console.log('Current location: ', location)
+
     //cameraService ist noch nicht fertig -> sollte formData zurueckgeben
     //der Tag wird dann noch mit 'location' hinzugefuegt
     this.cameraService.takeNewPicture();
