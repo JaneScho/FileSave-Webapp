a7754abea932c3727897737851ea43552f4acb08
jschophu <jannes.schophuis@hs-osnabrueck.de>
2026-02-21 18:31:08 +0100

    basic tag search component

--- src/app/components/file-popup/file-popup.component.ts
+++ src/app/components/file-popup/file-popup.component.ts
@@ -1,22 +1,26 @@
 import { asNativeElements, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { FileListDTO } from 'src/app/services/interfaces/dtos';
 import { TagInputLabelComponent } from "../tag-input-label/tag-input-label.component";
+import { SearchingTextInputComponent } from "../searching-text-input/searching-text-input.component";
 
 @Component({
   selector: 'page-file-popup',
   templateUrl: './file-popup.component.html',
   styleUrls: ['./file-popup.component.scss'],
-  imports: [TagInputLabelComponent],
+  imports: [TagInputLabelComponent, SearchingTextInputComponent],
   host:{
     class: 'w-100 popup display-flex'
   }
 })
 export class FilePopupComponent  implements OnInit {
   @Input('file-data') fileData: FileListDTO = {filename: '', filepath:'/', isFolder: false, tags:[]}
+  tagOptions: String[] = ['Testtags', 'Muss geladen werden'];
 
   constructor(private cdr: ChangeDetectorRef) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //TODO Retrieve possible tags for tagOptions
+  }
 
   removeTag(tagIndex: Number){
 
