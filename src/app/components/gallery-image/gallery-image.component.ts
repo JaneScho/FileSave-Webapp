4b3f324947b1154da5e338fecdb9ed66118613fa
jschophu <jannes.schophuis@hs-osnabrueck.de>
2026-02-22 03:05:50 +0100

    Anzeigen von Bildern in Galerie klappt

--- src/app/components/gallery-image/gallery-image.component.ts
+++ src/app/components/gallery-image/gallery-image.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { IonImg, IonSpinner } from "@ionic/angular/standalone";
 import { FileListDTO } from 'src/app/services/interfaces/dtos';
 import { api } from 'src/app/services/api/api';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'cmp-gallery-image',
@@ -9,21 +10,34 @@ import { api } from 'src/app/services/api/api';
   styleUrls: ['./gallery-image.component.scss'],
   imports: [IonSpinner, IonImg],
 })
-export class GalleryImageComponent  implements OnInit {
+export class GalleryImageComponent implements OnInit {
   @Input('fileData') fileData !: FileListDTO;
-  isLoading :Boolean = true;
+  
+  imageSrc!: string;
+  isLoading: Boolean = true;
   api = api.path + '/download/gallery/'
-  constructor(private cdr: ChangeDetectorRef) { }
 
-  ngOnInit() {}
 
-  imageLoaded(){
+  constructor(private cdr: ChangeDetectorRef, private http: HttpClient) { }
+
+  ngOnInit() {
+    const url = api.path + '/download/gallery/' + this.fileData.filename;
+
+    this.http.get(url, { responseType: 'blob' })
+      .subscribe(blob => {
+        this.imageSrc = URL.createObjectURL(blob);
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      });
+  }
+
+  imageLoaded() {
     this.isLoading = false;
     console.log("Done loading");
     this.cdr.detectChanges();
   }
 
-  imageLoadErr(){
+  imageLoadErr() {
     this.isLoading = false;
     console.error("Konnte Bild nicht laden");
     this.cdr.detectChanges();
