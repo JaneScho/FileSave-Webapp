65438bd68f4edc449df507c71e4735cff62af0fa
cwestenb <chantal.westenberg@hs-osnabrueck.de>
2026-02-22 15:53:48 +0100

    Im generellen sollte jetzt alles bei shared funktionieren

--- src/app/services/api/bearbeiten.ts
+++ src/app/services/api/bearbeiten.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { TagInputDTO } from '../interfaces/dtos';
 import { api } from './api';
+import { PathCorService } from '../dataCorrection/path-cor-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Bearbeiten {
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, 
+    private pathCorService: PathCorService
+  ){}
 
   updateTags(type: 'shared' | 'gallery' | 'files', filename: string, tags: TagInputDTO[], subPath?: string){
     const url = `${api.path}/bearbeiten/${type}/${encodeURIComponent(filename)}`;
@@ -17,13 +20,16 @@ export class Bearbeiten {
 
     let params = new HttpParams();
     if(subPath){
-      let cleanPath = subPath;
+      let cleanPath = this.pathCorService.cleanupPath(type, subPath);
 
+      /*
       if (subPath.includes('/files/')) {
         cleanPath = subPath.split('/files/')[1];
       } else if (subPath.endsWith('/files')) {
         cleanPath = '/';
       }
+        */
+       //'jannes/fotos/'
         params = params.set('p', cleanPath);
     }
     return this.http.put(url, tags, {params});
@@ -34,13 +40,15 @@ export class Bearbeiten {
 
     let params = new HttpParams();
     if(subPath){
-      let cleanPath = subPath;
+      let cleanPath = this.pathCorService.cleanupPath(type, subPath);
 
+      /*
       if (subPath.includes('/files/')) {
         cleanPath = subPath.split('/files/')[1];
       } else if (subPath.endsWith('/files')) {
         cleanPath = '/';
       }
+        */
         params = params.set('p', cleanPath);
     }
 
